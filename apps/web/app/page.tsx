7bbc15f2599e9b4090201072410801f4501aab16
Jarl-Erik Malmström <jarl-erik.malmstrom@wunderdog.fi>
2023-04-14 20:00:11 +0300

    Use the nextjs appDir

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.root}>
+      <h1>Welcome to web!</h1>
+      <h2>This is the way</h2>
+      <h3>Of the development</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+    </div>
+  );
+}
