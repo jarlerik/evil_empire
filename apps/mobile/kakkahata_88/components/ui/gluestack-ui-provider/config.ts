df1ada5a1532891357e70223b137e96bc2eadc2c
Jarl-Erik (Hoosee) Malmström <jarl-erik.malmstrom@wunderdog.fi>
2025-03-17 09:17:41 +0200

    Init gluestack-ui

---
+++ apps/mobile/kakkahata_88/components/ui/gluestack-ui-provider/config.ts
@@ -0,0 +1,18 @@
+import { createConfig } from '@gluestack-ui/themed';
+
+export const config = createConfig({
+	tokens: {
+		colors: {
+			primary: '#0891B2',
+			secondary: '#64748B',
+			tertiary: '#CBD5E1'
+		}
+	},
+	aliases: {
+		colors: {
+			primary: 'primary',
+			secondary: 'secondary',
+			tertiary: 'tertiary'
+		}
+	}
+});
