51fca3ecd4b7ae0efa00c47e1949b3842d75bb75
Jarl-Erik (Hoosee) Malmström <jarl-erik.malmstrom@wunderdog.fi>
2025-04-12 19:30:14 +0300

    Remove blur view

--- components/ui/TabBarBackground.ios.tsx
+++ components/ui/TabBarBackground.ios.tsx
@@ -1,20 +1,16 @@
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import { BlurView } from 'expo-blur';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function BlurTabBarBackground() {
   const insets = useSafeAreaInsets();
   
   return (
-    <BlurView
-      // System chrome material automatically adapts to the system's theme
-      // and matches the native tab bar appearance on iOS.
-      tint="systemChromeMaterial"
-      intensity={100}
+    <View 
       style={[
         StyleSheet.absoluteFill,
         {
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
           paddingBottom: Math.max(insets.bottom, 0)
         }
       ]}
