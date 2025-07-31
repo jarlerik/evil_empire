5563f603cc8032a103e4f7c31af4e85e0c3b874f
Jarl-Erik (Hoosee) Malmström <jarl-erik.malmstrom@wunderdog.fi>
2025-07-31 15:49:23 +0300

    Remove expo-router/babel

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Required for expo-router
-      'expo-router/babel',
-      // Add react-native-reanimated plugin
       'react-native-reanimated/plugin',
     ],
   };
