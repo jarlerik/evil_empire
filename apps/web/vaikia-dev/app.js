c4018dde78c6c5bd9ff8efcc6f2e662ed5ef8f2c
Jarl-Erik (Hoosee) Malmström <jarl-erik.malmstrom@wunderdog.fi>
2026-01-27 18:18:24 +0200

    Try some animejs stuff

--- apps/web/vaikia-dev/app.js
+++ apps/web/vaikia-dev/app.js
@@ -1,1 +1,20 @@
-console.log('Vaikia.dev web app loaded');
+import { animate, stagger, splitText } from 'https://esm.sh/animejs';
+
+const { chars } = splitText('h1', { words: false, chars: true });
+
+animate(chars, {
+  // Property keyframes
+  y: [
+    { to: '-2.75rem', ease: 'outExpo', duration: 600 },
+    { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
+  ],
+  // Property specific parameters
+  rotate: {
+    from: '-1turn',
+    delay: 0
+  },
+  delay: stagger(50),
+  ease: 'inOutCirc',
+  loopDelay: 1000,
+  loop: true
+});
